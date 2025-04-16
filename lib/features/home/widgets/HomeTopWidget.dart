da862e2e8738ae13d15f6a682cb023fb30e8d0be
heui-yong <gusrnr9878@gmail.com>
2025-04-16 16:07:16 +0900

    [home] : top 영역 작업  - 타이틀문구, 이름, 대표 이미지, 시간, 장소 추가

---
+++ lib/features/home/widgets/HomeTopWidget.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/common/common.dart';
+
+class HomeTopWidget extends StatelessWidget {
+  const HomeTopWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 32),
+        const Text(
+          AppString.homeTitle,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontFamily: "Francisco", fontSize: 24, color: AppColor.color_9E9E9E),
+        ),
+        const SizedBox(height: 14),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Column(
+              children: [
+                Text(
+                  AppString.groomNameEn,
+                  style: TextStyle(
+                      fontFamily: "Francisco",
+                      fontSize: 14,
+                      color: AppColor.color_9E9E9E),
+                ),
+                SizedBox(height: 4),
+                Text(
+                  AppString.groomNameKo,
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: AppColor.color_9E9E9E),
+                )
+              ],
+            ),
+            const SizedBox(width: 60),
+            Container(
+              width: 0.5,
+              height: 20,
+              color: AppColor.color_9E9E9E,
+            ),
+            const SizedBox(width: 60),
+            const Column(
+              children: [
+                Text(
+                  AppString.brideNameEn,
+                  style: TextStyle(
+                      fontFamily: "Francisco",
+                      fontSize: 14,
+                      color: AppColor.color_9E9E9E),
+                ),
+                SizedBox(height: 4),
+                Text(
+                  AppString.brideNameKo,
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: AppColor.color_9E9E9E),
+                )
+              ],
+            ),
+          ],
+        ),
+        const SizedBox(height: 14,),
+        Flexible(
+          child: Image.asset(
+            AppImagePath.homeTitlePath,
+            width: MediaQuery.of(context).size.width * 0.6,
+            fit: BoxFit.contain,
+          ),
+        ),
+        const SizedBox(height: 14,),
+        const Text(
+          AppString.marryDate,
+          style: TextStyle(
+              fontSize: 14,
+              color: AppColor.color_9E9E9E),
+        ),
+        const Text(
+          AppString.marryPlace,
+          style: TextStyle(
+              fontSize: 14,
+              color: AppColor.color_9E9E9E),
+        ),
+      ],
+    );
+  }
+}
