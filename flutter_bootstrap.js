6f964ebd30cb99222fe4e5ac225bfd256c6fe658
heui-yong <heui-yong@users.noreply.github.com>
2025-04-22 07:25:27 +0000

    deploy: 95826deff3ebc3b8dde50c5b70b23ed920623c3f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2413545120"
+    serviceWorkerVersion: "3005686387"
   }
 });
