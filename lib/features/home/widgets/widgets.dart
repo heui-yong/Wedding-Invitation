8a4395f1c74d157394290764fa46ed4892ae59f9
heui-yong <gusrnr9878@gmail.com>
2025-04-22 14:45:59 +0900

    [home] : 연락 위젯 구현중

--- lib/features/home/widgets/widgets.dart
+++ lib/features/home/widgets/widgets.dart
@@ -3,3 +3,4 @@ export 'home_gallery_widget.dart';
 export 'home_gallery_widget_2.dart';
 export 'gallery_dialog.dart';
 export 'invitation_message_widget.dart';
+export 'home_contact_widget.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       theme: ThemeData(
-        fontFamily: "Komi"
+        fontFamily: "MaruBuri"
       ),
       routerConfig: appRouter,
     );
