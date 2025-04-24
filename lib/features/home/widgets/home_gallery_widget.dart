5835e97383a47afdf404d6c72e4689c9b4c73b53
heui-yong <gusrnr9878@gmail.com>
2025-04-24 12:35:20 +0900

    [issue-23] : 갤러리 펼치고 닫을때 애니메이션이 버벅이는 현상 #23  - 수정

--- lib/features/home/widgets/home_gallery_widget.dart
+++ lib/features/home/widgets/home_gallery_widget.dart
@@ -19,6 +19,8 @@ class HomeGalleryWidget extends ConsumerStatefulWidget {
 class _HomeGalleryWidgetState extends ConsumerState<HomeGalleryWidget>
     with TickerProviderStateMixin {
   bool _isExpanded = false;
+  late AnimationController _animationController;
+  late Animation<double> _heightAnimation;
 
   int _currentIndex = 0;
   late final PageController _pageController;
@@ -28,11 +30,22 @@ class _HomeGalleryWidgetState extends ConsumerState<HomeGalleryWidget>
     super.initState();
     _pageController =
         PageController(viewportFraction: 0.2, initialPage: _currentIndex);
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _heightAnimation = Tween<double>(begin: 600, end: 1500).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
   }
 
   @override
   void dispose() {
     _pageController.dispose();
+    _animationController.dispose();
     super.dispose();
   }
 
@@ -50,92 +63,102 @@ class _HomeGalleryWidgetState extends ConsumerState<HomeGalleryWidget>
         Text(AppString.homeGalleryTitle,
             style: TextStyle(fontSize: 18, color: AppColor.color_9E9E9E)),
         const SizedBox(height: 12),
-        AnimatedSize(
-          duration: const Duration(milliseconds: 300),
-          child: ClipRect(
-            child: _isExpanded
-                ? Column(
-                    children: [
-                      // 3개씩 묶어서 반복 생성
-                      ...List.generate(rowCount, (rowIdx) {
-                        final start = rowIdx * 3;
-                        final chunk = widget.galleryImageList1to2.sublist(
-                            start,
-                            start + 3 > widget.galleryImageList1to2.length
-                                ? widget.galleryImageList1to2.length
-                                : start + 3);
-                        return _buildGalleryRow(chunk,
-                            largeLeft: rowIdx.isEven);
-                      }),
-                      ...List.generate(rowCount1, (rowIdx) {
-                        final start = rowIdx * 4;
-                        final chunk = widget.galleryImageList1to1.sublist(
-                            start,
-                            start + 4 > widget.galleryImageList1to1.length
-                                ? widget.galleryImageList1to1.length
-                                : start + 4);
-                        return _buildGalleryRow2(chunk,
-                            largeLeft: rowIdx.isEven);
-                      }),
+        Stack(
+          children: [
+            AnimatedBuilder(
+              animation: _animationController,
+              builder: (context, child) {
+                return Container(
+                  height: _isExpanded
+                      ? _heightAnimation.value
+                      : 600, // Show partial content when collapsed
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                  ),
+                  child: SingleChildScrollView(
+                    physics: const NeverScrollableScrollPhysics(),
+                    child: child,
+                  ),
+                );
+              },
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ...List.generate(rowCount, (rowIdx) {
+                    final start = rowIdx * 3;
+                    final chunk = widget.galleryImageList1to2.sublist(
+                        start,
+                        start + 3 > widget.galleryImageList1to2.length
+                            ? widget.galleryImageList1to2.length
+                            : start + 3);
+                    return _buildGalleryRow(chunk, largeLeft: rowIdx.isEven);
+                  }),
+                  ...List.generate(rowCount1, (rowIdx) {
+                    final start = rowIdx * 4;
+                    final chunk = widget.galleryImageList1to1.sublist(
+                        start,
+                        start + 4 > widget.galleryImageList1to1.length
+                            ? widget.galleryImageList1to1.length
+                            : start + 4);
+                    return _buildGalleryRow2(chunk, largeLeft: rowIdx.isEven);
+                  }),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                padding: const EdgeInsets.only(top: 20.0),
+                decoration: BoxDecoration(
+                  gradient: _isExpanded ? null : LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.white.withOpacity(0.0),
+                      Colors.white.withOpacity(0.8),
+                      Colors.white,
                     ],
                   )
-                : ClipRect(
-                    child: ConstrainedBox(
-                      constraints: const BoxConstraints(maxHeight: 560),
-                      child: SingleChildScrollView(
-                        physics: const NeverScrollableScrollPhysics(),
-                        child: Column(
-                          children: [
-                            // 3개씩 묶어서 반복 생성
-                            ...List.generate(rowCount, (rowIdx) {
-                              final start = rowIdx * 3;
-                              final chunk = widget.galleryImageList1to2.sublist(
-                                  start,
-                                  start + 3 > widget.galleryImageList1to2.length
-                                      ? widget.galleryImageList1to2.length
-                                      : start + 3);
-                              return _buildGalleryRow(chunk,
-                                  largeLeft: rowIdx.isEven);
-                            }),
-                            ...List.generate(rowCount1, (rowIdx) {
-                              final start = rowIdx * 4;
-                              final chunk = widget.galleryImageList1to1.sublist(
-                                  start,
-                                  start + 4 > widget.galleryImageList1to1.length
-                                      ? widget.galleryImageList1to1.length
-                                      : start + 4);
-                              return _buildGalleryRow2(chunk,
-                                  largeLeft: rowIdx.isEven);
-                            }),
-                          ],
+                ),
+                child: Center(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _isExpanded = !_isExpanded;
+                        if (_isExpanded) {
+                          _animationController.forward();
+                        } else {
+                          _animationController.reverse();
+                        }
+                      });
+                    },
+                    child: Container(
+                      width: 160,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                            color: AppColor.color_5A5555.withOpacity(0.5),
+                            width: 1),
+                      ),
+                      child: Center(
+                        child: Text(
+                          _isExpanded ? "접기" : "더 보기",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: AppColor.color_5A5555.withOpacity(0.9),
+                          ),
+                          textAlign: TextAlign.center,
                         ),
                       ),
                     ),
                   ),
-          ),
-        ),
-        SizedBox(height: 18,),
-        GestureDetector(
-          onTap: () => setState(() => _isExpanded = !_isExpanded),
-          child: Container(
-            width: 160,
-            height: 40,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(
-                  color: AppColor.color_5A5555.withOpacity(0.5), width: 1),
-            ),
-            child: Center(
-              child: Text(
-                _isExpanded ? "접기" : "더 보기",
-                style: TextStyle(
-                  fontSize: 15,
-                  color: AppColor.color_5A5555.withOpacity(0.9),
                 ),
-                textAlign: TextAlign.center,
               ),
             ),
-          ),
+          ],
         ),
       ],
     );
