8fc56e41e188c660d67509120038b1f97174ce29
heui-yong <gusrnr9878@gmail.com>
2025-04-28 13:51:39 +0900

    [map] : 지도 위젯 영역 구현 완료

--- lib/features/home/widgets/home_map_widget.dart
+++ lib/features/home/widgets/home_map_widget.dart
@@ -64,14 +64,18 @@ class _HomeMapWidgetState extends State<HomeMapWidget> {
               height: 40,
               child: Row(
                 children: [
-                  Expanded(child: buildMapMethod(AppImagePath.naverMapPath, "네이버 지도")),
+                  Expanded(
+                      child:
+                          buildMapMethod(AppImagePath.naverMapPath, "네이버 지도")),
                   VerticalDivider(
                     color: AppColor.color_9E9E9E,
                     thickness: 1,
                     indent: 10,
                     endIndent: 10,
                   ),
-                  Expanded(child: buildMapMethod(AppImagePath.kakaoMapPath, "카카오 내비")),
+                  Expanded(
+                      child:
+                          buildMapMethod(AppImagePath.kakaoMapPath, "카카오 내비")),
                   VerticalDivider(
                     color: AppColor.color_9E9E9E,
                     thickness: 1,
@@ -83,10 +87,155 @@ class _HomeMapWidgetState extends State<HomeMapWidget> {
               ),
             ),
             SizedBox(
-              height: 12,
+              height: 24,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 36,
+                    height: 36,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle, color: AppColor.color_F0F0F0),
+                    child: Icon(
+                      Icons.train_outlined,
+                      color: AppColor.color_9E9E9E,
+                      size: 21,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "지하철",
+                        style: TextStyle(
+                            color: AppColor.color_5A5555, fontSize: 18),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Text.rich(
+                        TextSpan(
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: AppColor.color_5A5555,
+                          ),
+                          children: [
+                            const TextSpan(
+                              text: '[2호선]',
+                              style: TextStyle(
+                                  color: AppColor.color_5A5555, fontSize: 12),
+                            ),
+                            const TextSpan(
+                              text: ' 센텀시티역',
+                              style: TextStyle(
+                                  color: AppColor.color_000000,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w700
+                              ),
+                            ),
+                            const TextSpan(
+                              text: ' 11번 출구 도보 3분',
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text.rich(
+                        TextSpan(
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: AppColor.color_5A5555,
+                          ),
+                          children: [
+                            const TextSpan(
+                              text: '[동해선]',
+                              style: TextStyle(
+                                  color: AppColor.color_5A5555, fontSize: 12),
+                            ),
+                            const TextSpan(
+                              text: ' 벡스코역',
+                              style: TextStyle(
+                                  color: AppColor.color_000000,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w700
+                              ),
+                            ),
+                            const TextSpan(
+                              text: ' 1번 또는 2호선 9번출구 도보 15분',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            // 구분선
+            SizedBox(height: 18),
+            Divider(color: AppColor.color_9E9E9E.withOpacity(0.5),height: 0.5,),
+            SizedBox(height: 18),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 36,
+                    height: 36,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle, color: AppColor.color_F0F0F0),
+                    child: Icon(
+                      Icons.directions_bus_filled_outlined,
+                      color: AppColor.color_9E9E9E,
+                      size: 21,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "버스",
+                        style: TextStyle(
+                            color: AppColor.color_5A5555, fontSize: 18),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        '신세계 센텀시티 정류장',
+                        style: TextStyle(
+                          color: AppColor.color_5A5555,
+                          fontSize: 12,
+                        ),
+                      ),
+                      Text(
+                        '5-1, 39, 40, 63, 115, 139, 141, 155, 181, 307, 1001, 1002, 1007',
+                        style: TextStyle(
+                            color: AppColor.color_5A5555,
+                            fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ],
         ),
+        const SizedBox(height: 36),
       ],
     );
   }
@@ -101,7 +250,9 @@ class _HomeMapWidgetState extends State<HomeMapWidget> {
             width: 18,
             height: 18,
           ),
-          const SizedBox(width: 12,),
+          const SizedBox(
+            width: 12,
+          ),
           Text(
             mapName,
             style: TextStyle(color: AppColor.color_000000, fontSize: 14),
